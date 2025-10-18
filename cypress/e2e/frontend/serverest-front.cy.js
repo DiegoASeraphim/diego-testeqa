578e90da4d96f5b34029ca7b20efb7589981089b
Diego Seraphim <156013137+DiegoASeraphim@users.noreply.github.com>
2025-10-18 14:26:12 -0300

    test: add frontend e2e test scenarios

---
+++ cypress/e2e/frontend/serverest-front.cy.js
@@ -0,0 +1,42 @@
+import LoginPage from '../../support/pageObjects/LoginPage'
+
+describe('Serverest Store - Frontend E2E', () => {
+  const loginPage = new LoginPage()
+
+  beforeEach(() => {
+    cy.visit('https://front.serverest.dev/login')
+  })
+
+  it('Deve realizar login com sucesso e exibir produtos', () => {
+    loginPage.preencherEmail('testediego@teste.com')
+    loginPage.preencherSenha('teste123')
+    loginPage.clicarEntrar()
+
+    cy.url().should('include', '/home')
+    cy.contains('Serverest Store').should('be.visible')
+    cy.contains('Adicionar a lista').should('exist')
+  })
+
+  it('Deve exibir mensagem de erro ao tentar login inválido', () => {
+    loginPage.preencherEmail('fulano@qa.com')
+    loginPage.preencherSenha('teste')
+    loginPage.clicarEntrar()
+
+    cy.contains('Email e/ou senha inválidos').should('be.visible')
+    cy.url().should('include', '/login')
+  })
+
+  it('Deve exibir lista de produtos após login', () => {
+    loginPage.login('testediego@teste.com', 'teste123')
+    cy.contains('Serverest Store').should('be.visible')
+    cy.get('.card').should('have.length.greaterThan', 0)
+  })
+
+  it('Deve adicionar um produto à lista com sucesso', () => {
+    loginPage.login('testediego@teste.com', 'teste123')
+    cy.contains('Adicionar a lista').first().click()
+    cy.on('window:alert', (msg) => {
+      expect(msg).to.contain('Produto adicionado com sucesso')
+    })
+  })
+})
